b2aa5c05399339ceccbbdb2dca5f706d7a0e1bfd
Tim Knapp <timknapp12@gmail.com>
2025-06-12 15:53:28 -0600

    update const with solana kit

--- packages/kit/src/consts.ts
+++ packages/kit/src/consts.ts
@@ -1,1 +1,5 @@
-//TODO fill out this file
+import { address, Address } from '@solana/kit';
+
+export const SWIG_PROGRAM_ADDRESS: Address = address(
+  'swigDk8JezhiAVde8k6NMwxpZfgGm2NNuMe1KYCmUjP',
+);
