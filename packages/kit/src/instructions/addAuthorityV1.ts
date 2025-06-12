4128b3c2c20fd1f8189e46ea0606dc267b8f88bc
Tim Knapp <timknapp12@gmail.com>
2025-06-12 17:23:49 -0600

    add authority v1

--- packages/kit/src/instructions/addAuthorityV1.ts
+++ packages/kit/src/instructions/addAuthorityV1.ts
@@ -1,1 +1,53 @@
-//TODO fill out this file
+import { AccountRole, Address, IAccountMeta } from '@solana/kit';
+
+export type AddAuthorityV1InstructionAccounts = {
+  swig: Address;
+  payer: Address;
+};
+
+export type AddAuthorityV1BaseAccountMetas = [
+  IAccountMeta,
+  IAccountMeta,
+  IAccountMeta,
+];
+
+export function getAddAuthorityV1BaseAccountMetas(
+  accounts: AddAuthorityV1InstructionAccounts,
+): AddAuthorityV1BaseAccountMetas {
+  return [
+    {
+      address: accounts.swig,
+      role: AccountRole.WRITABLE, // isSigner: false, isWritable: true
+    },
+    {
+      address: accounts.payer,
+      role: AccountRole.WRITABLE_SIGNER, // isSigner: true, isWritable: true
+    },
+    {
+      address: '11111111111111111111111111111111' as Address,
+      role: AccountRole.READONLY, // isSigner: false, isWritable: false
+    },
+  ];
+}
+
+export type AddAuthorityV1BaseAccountMetasWithAuthority = [
+  ...AddAuthorityV1BaseAccountMetas,
+  IAccountMeta,
+];
+
+export function getAddV1BaseAccountMetasWithAuthority(
+  accounts: AddAuthorityV1InstructionAccounts,
+  authority: Address,
+): [AddAuthorityV1BaseAccountMetasWithAuthority, number] {
+  const accountMetas = getAddAuthorityV1BaseAccountMetas(accounts);
+  const authorityIndex = accountMetas.length;
+
+  const metas: AddAuthorityV1BaseAccountMetasWithAuthority = [
+    ...accountMetas,
+    {
+      address: authority,
+      role: AccountRole.READONLY_SIGNER, // isSigner: true, isWritable: false
+    },
+  ];
+  return [metas, authorityIndex];
+}
