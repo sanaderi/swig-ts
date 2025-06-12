a4dc07e3f2ab3358cfe561ffd91002b328c952fa
Tim Knapp <timknapp12@gmail.com>
2025-06-12 16:19:17 -0600

    add wallet.ts and interface signingIn funct

--- packages/kit/src/authority/instructions/interface.ts
+++ packages/kit/src/authority/instructions/interface.ts
@@ -1,1 +1,10 @@
-//TODO fill out this file
+/**
+ * Signing interface that takes a message and returns a signature of the signed message
+ */
+export type SigningFn = (message: Uint8Array) => Promise<SigningResult>;
+
+/**
+ * @property signature - Signature of the message
+ * @property prefix - Additional Prefix added to the message
+ */
+export type SigningResult = { signature: Uint8Array; prefix?: Uint8Array };
