d7b027a069d2e51db1e7c3401b9efa853e2bbbb8
Tim Knapp <timknapp12@gmail.com>
2025-06-17 07:42:25 -0600

    copy files from classic to kit from authority

--- packages/kit/src/authority/ed25519/token.ts
+++ packages/kit/src/authority/ed25519/token.ts
@@ -1,1 +1,262 @@
-//TODO fill out this file
+import {
+  getAssociatedTokenAddressSync,
+  TOKEN_PROGRAM_ID,
+} from '@solana/spl-token';
+import { PublicKey, TransactionInstruction } from '@solana/web3.js';
+import { AuthorityType } from '@swig-wallet/coder';
+import type { Actions } from '../../actions';
+import { createSwigInstruction } from '../../instructions';
+import { findSwigSubAccountPda } from '../../utils';
+import { Authority, TokenBasedAuthority } from '../abstract';
+import type { CreateAuthorityInfo } from '../createAuthority';
+import { Ed25519Instruction } from '../instructions';
+import type { Ed25519BasedAuthority } from './based';
+
+export class Ed25519Authority
+  extends TokenBasedAuthority
+  implements Ed25519BasedAuthority
+{
+  type = AuthorityType.Ed25519;
+
+  constructor(data: Uint8Array, roleId?: number) {
+    super(data, roleId ?? null);
+  }
+
+  static fromPublicKey(publicKey: PublicKey): Ed25519Authority {
+    return new Ed25519Authority(publicKey.toBytes());
+  }
+
+  get id() {
+    return this.data;
+  }
+
+  get signer() {
+    return this.data;
+  }
+
+  get publicKey() {
+    return this.ed25519PublicKey;
+  }
+
+  get address() {
+    return this.ed25519PublicKey;
+  }
+
+  get ed25519PublicKey() {
+    return new PublicKey(this.data);
+  }
+
+  createAuthorityData() {
+    return this.data;
+  }
+
+  create(args: {
+    payer: PublicKey;
+    id: Uint8Array;
+    actions: Actions;
+  }): TransactionInstruction {
+    return createSwigInstruction(
+      { payer: args.payer },
+      {
+        authorityData: this.createAuthorityData(),
+        id: args.id,
+        actions: args.actions.bytes(),
+        authorityType: this.type,
+        noOfActions: args.actions.count,
+      },
+    );
+  }
+
+  sign(args: {
+    swigAddress: PublicKey;
+    payer: PublicKey;
+    roleId: number;
+    innerInstructions: TransactionInstruction[];
+  }) {
+    return Ed25519Instruction.signV1Instruction(
+      {
+        swig: args.swigAddress,
+        payer: args.payer,
+      },
+      {
+        authorityData: this.data,
+        innerInstructions: args.innerInstructions,
+        roleId: args.roleId,
+      },
+    );
+  }
+
+  addAuthority(args: {
+    swigAddress: PublicKey;
+    payer: PublicKey;
+    actingRoleId: number;
+    actions: Actions;
+    newAuthorityInfo: CreateAuthorityInfo;
+  }) {
+    return Ed25519Instruction.addAuthorityV1Instruction(
+      {
+        payer: args.payer,
+        swig: args.swigAddress,
+      },
+      {
+        actingRoleId: args.actingRoleId,
+        actions: args.actions.bytes(),
+        authorityData: this.data,
+        newAuthorityData: args.newAuthorityInfo.createAuthorityInfo.data,
+        newAuthorityType: args.newAuthorityInfo.createAuthorityInfo.type,
+        noOfActions: args.actions.count,
+      },
+    );
+  }
+
+  removeAuthority(args: {
+    payer: PublicKey;
+    swigAddress: PublicKey;
+    roleId: number;
+    roleIdToRemove: number;
+  }) {
+    return Ed25519Instruction.removeAuthorityV1Instruction(
+      {
+        payer: args.payer,
+        swig: args.swigAddress,
+      },
+      {
+        actingRoleId: args.roleId,
+        authorityData: this.data,
+        authorityToRemoveId: args.roleIdToRemove,
+      },
+    );
+  }
+
+  subAccountCreate(args: {
+    payer: PublicKey;
+    swigAddress: PublicKey;
+    swigId: Uint8Array;
+    roleId: number;
+  }) {
+    const [subAccount, bump] = findSwigSubAccountPda(args.swigId, args.roleId);
+    return Ed25519Instruction.subAccountCreateV1Instruction(
+      {
+        payer: args.payer,
+        swig: args.swigAddress,
+        subAccount,
+      },
+      {
+        roleId: args.roleId,
+        authorityData: this.data,
+        bump,
+      },
+    );
+  }
+
+  subAccountSign(args: {
+    payer: PublicKey;
+    swigAddress: PublicKey;
+    subAccount: PublicKey;
+    roleId: number;
+    innerInstructions: TransactionInstruction[];
+  }) {
+    return Ed25519Instruction.subAccountSignV1Instruction(
+      {
+        payer: args.payer,
+        swig: args.swigAddress,
+        subAccount: args.subAccount,
+      },
+      {
+        roleId: args.roleId,
+        authorityData: this.data,
+        innerInstructions: args.innerInstructions,
+      },
+    );
+  }
+
+  subAccountToggle(args: {
+    payer: PublicKey;
+    swigAddress: PublicKey;
+    subAccount: PublicKey;
+    roleId: number;
+    enabled: boolean;
+  }) {
+    return Ed25519Instruction.subAccountToggleV1Instruction(
+      {
+        payer: args.payer,
+        swig: args.swigAddress,
+        subAccount: args.subAccount,
+      },
+      {
+        roleId: args.roleId,
+        authorityData: this.data,
+        enabled: args.enabled,
+      },
+    );
+  }
+
+  subAccountWithdrawSol(args: {
+    payer: PublicKey;
+    swigAddress: PublicKey;
+    subAccount: PublicKey;
+    roleId: number;
+    amount: bigint;
+  }) {
+    return Ed25519Instruction.subAccountWithdrawV1SolInstruction(
+      {
+        payer: args.payer,
+        swig: args.swigAddress,
+        subAccount: args.subAccount,
+      },
+      {
+        roleId: args.roleId,
+        authorityData: this.data,
+        amount: args.amount,
+      },
+    );
+  }
+
+  subAccountWithdrawToken(args: {
+    payer: PublicKey;
+    swigAddress: PublicKey;
+    subAccount: PublicKey;
+    roleId: number;
+    mint: PublicKey;
+    amount: bigint;
+    tokenProgram?: PublicKey;
+  }) {
+    const swigToken = getAssociatedTokenAddressSync(
+      args.mint,
+      args.swigAddress,
+      true,
+      args.tokenProgram,
+    );
+    const subAccountToken = getAssociatedTokenAddressSync(
+      args.mint,
+      args.subAccount,
+      true,
+      args.tokenProgram,
+    );
+    return Ed25519Instruction.subAccountWithdrawV1TokenInstruction(
+      {
+        payer: args.payer,
+        swig: args.swigAddress,
+        subAccount: args.subAccount,
+        subAccountToken,
+        swigToken,
+        tokenProgram: args.tokenProgram ?? TOKEN_PROGRAM_ID,
+      },
+      {
+        roleId: args.roleId,
+        authorityData: this.data,
+        amount: args.amount,
+      },
+    );
+  }
+}
+
+export function getEd25519AuthorityFromPublicKey(publicKey: PublicKey) {
+  return Ed25519Authority.fromPublicKey(publicKey);
+}
+
+export function isEd25519Authority(
+  authority: Authority,
+): authority is Ed25519Authority {
+  return authority instanceof Ed25519Authority;
+}
--- packages/kit/src/authority/instructions/interface.ts
+++ packages/kit/src/authority/instructions/interface.ts
@@ -1,3 +1,105 @@
+import type { ReadonlyUint8Array } from '@solana/kit';
+import type { TransactionInstruction } from '@solana/web3.js';
+import type {
+  AddAuthorityV1InstructionDataArgs,
+  CreateSessionV1InstructionDataArgs,
+  RemoveAuthorityV1InstructionDataArgs,
+  SubAccountCreateV1InstructionDataArgs,
+  SubAccountToggleV1InstructionDataArgs,
+  SubAccountWithdrawV1InstructionDataArgs,
+} from '@swig-wallet/coder';
+import {
+  type AddAuthorityV1InstructionAccounts,
+  type RemoveAuthorityV1InstructionAccounts,
+  type SignV1InstructionAccounts,
+  type SubAccountCreateV1InstructionAccounts,
+  type SubAccountSignV1InstructionAccounts,
+  type SubAccountToggleV1InstructionAccounts,
+  type SubAccountWithdrawV1SolInstructionAccounts,
+  type SubAccountWithdrawV1TokenInstructionAccounts,
+} from '../../instructions';
+
+/**
+ * Authority Instruction Interface
+ */
+export interface AuthorityInstruction {
+  addAuthorityV1Instruction(
+    accounts: AddAuthorityV1InstructionAccounts,
+    data: Omit<AddAuthorityV1InstructionDataArgs, 'authorityPayload'> & {
+      authorityData: ReadonlyUint8Array;
+    },
+    options?: InstructionDataOptions,
+  ): Promise<TransactionInstruction>;
+
+  removeAuthorityV1Instruction(
+    accounts: RemoveAuthorityV1InstructionAccounts,
+    data: Omit<RemoveAuthorityV1InstructionDataArgs, 'authorityPayload'> & {
+      authorityData: ReadonlyUint8Array;
+    },
+    options?: InstructionDataOptions,
+  ): Promise<TransactionInstruction>;
+
+  signV1Instruction(
+    accounts: SignV1InstructionAccounts,
+    data: {
+      authorityData: ReadonlyUint8Array;
+      roleId: number;
+      innerInstructions: TransactionInstruction[];
+    },
+    options?: InstructionDataOptions,
+  ): Promise<TransactionInstruction>;
+
+  createSessionV1Instruction(
+    accounts: SignV1InstructionAccounts,
+    data: Omit<CreateSessionV1InstructionDataArgs, 'authorityPayload'> & {
+      authorityData: ReadonlyUint8Array;
+    },
+    options?: InstructionDataOptions,
+  ): Promise<TransactionInstruction>;
+
+  subAccountCreateV1Instruction(
+    accounts: SubAccountCreateV1InstructionAccounts,
+    data: Omit<SubAccountCreateV1InstructionDataArgs, 'authorityPayload'> & {
+      authorityData: ReadonlyUint8Array;
+    },
+    options?: InstructionDataOptions,
+  ): Promise<TransactionInstruction>;
+
+  subAccountWithdrawV1SolInstruction(
+    accounts: SubAccountWithdrawV1SolInstructionAccounts,
+    data: Omit<SubAccountWithdrawV1InstructionDataArgs, 'authorityPayload'> & {
+      authorityData: ReadonlyUint8Array;
+    },
+    options?: InstructionDataOptions,
+  ): Promise<TransactionInstruction>;
+
+  subAccountWithdrawV1TokenInstruction(
+    accounts: SubAccountWithdrawV1TokenInstructionAccounts,
+    data: Omit<SubAccountWithdrawV1InstructionDataArgs, 'authorityPayload'> & {
+      authorityData: ReadonlyUint8Array;
+    },
+    options?: InstructionDataOptions,
+  ): Promise<TransactionInstruction>;
+
+  subAccountToggleV1Instruction(
+    accounts: SubAccountToggleV1InstructionAccounts,
+    data: Omit<SubAccountToggleV1InstructionDataArgs, 'authorityPayload'> & {
+      authorityData: ReadonlyUint8Array;
+    },
+    options?: InstructionDataOptions,
+  ): Promise<TransactionInstruction>;
+
+  subAccountSignV1Instruction(
+    accounts: SubAccountSignV1InstructionAccounts,
+    data: {
+      authorityData: ReadonlyUint8Array;
+      roleId: number;
+      innerInstructions: TransactionInstruction[];
+    },
+    options?: InstructionDataOptions,
+  ): Promise<TransactionInstruction>;
+}
+
 /**
  * Signing interface that takes a message and returns a signature of the signed message
  */
@@ -8,3 +110,14 @@ export type SigningFn = (message: Uint8Array) => Promise<SigningResult>;
  * @property prefix - Additional Prefix added to the message
  */
 export type SigningResult = { signature: Uint8Array; prefix?: Uint8Array };
+
+/**
+ * Options used for constructing or signing instruction data.
+ *
+ * @property signingFn - {@link SigningFn}.
+ * @property currentSlot - current slot.
+ */
+export type InstructionDataOptions = {
+  signingFn: SigningFn;
+  currentSlot: bigint;
+};
